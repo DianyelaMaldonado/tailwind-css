18dc7a89d710cf247f68c78c692c9244f935490d
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2024-02-12 13:55:23 -0600

    Added simple hero building in tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], //tailwind trabaja con un elemento llamdo -> jit(just in time -permite agregar clases personalizadas directo en el html y me permite cargarlas automaticamente en la hoja de estilos)
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
